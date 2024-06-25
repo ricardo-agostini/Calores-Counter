8b3b8c6760acf88e9f5b45d6ac668ad43324f8b6
ricardo-agostini <ricardoagneto@gmail.com>
2024-06-25 09:33:42 -0300

    first commit

---
+++ CaloriesCounter/CaloriesCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesCounterApp.swift
+//  CaloriesCounter
+//
+//  Created by Ricardo de Agostini Neto on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaloriesCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaloriesCounter
+//
+//  Created by Ricardo de Agostini Neto on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
